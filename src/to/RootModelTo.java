c079b476051bc81c4d456c48e5df55fb02888a79
admin@javaops.ru <gitlaB13>
2021-04-25 11:53:32 +0300

    update

--- src/to/RootModelTo.java
+++ src/to/RootModelTo.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 // Группа технологий со своим кодом
 public class RootModelTo {
+    public RootModelTo(String rootCode) {
+        this.rootCode = rootCode;
+    }
+
     public String rootCode;
     public List<TechModelTo> techList;
 }
